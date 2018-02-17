0298ad3f94e206d3d4849156058155bb52a7f398
Denis <35633718+DenisGubeev@users.noreply.github.com>
2018-02-17 09:07:37 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,48 @@
+<?php
+if (isset($_POST['upload'])) {
+	if (isset($_FILES['myfile'])) {
+    $file = $_FILES['myfile'];
+	}
+	if (!empty(glob('tests/*.json'))) {
+        $allFiles = glob('tests/*.json');
+		} else {
+        $allFiles = [0];
+		}
+	$uploadfile = 'tests/' . basename($_FILES['myfile']['name']);
+		if (!empty($file['name']) && $file['error'] == UPLOAD_ERR_OK && 
+		move_uploaded_file($file['tmp_name'],$uploadfile)) {
+			echo 'Файл загружен';
+	} else {
+		echo 'Файл не загружен, попробуйте еще раз.';
+	}
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php if (!isset($_POST['create']) && !isset($_POST['upload'])): ?>
+    <form  method="POST" enctype="multipart/form-data">
+        <fieldset>
+            <legend>Загрузите свой тест в формате json</legend>
+            <input type="file" name="myfile" required>
+            <input type="submit" value="Добавить в базу" name="upload">
+        </fieldset>
+    </form>
+<?php endif; ?>
+<?php if (isset($_POST['upload'])): ?>
+    <h1>Служебная информация:</h1>
+    <pre>
+        <?php print_r($_FILES); ?>
+    </pre>
+ <?php endif; ?>
+    <div>
+        <fieldset>
+            <a href="list.php">Посмотреть все тесты >></a>
+        </fieldset>
+    </div>
+</body>
+</html>
